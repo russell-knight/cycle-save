a7b356cef0d3e01ec8f30bfec9637e95c3478983
Russell Knight <rknigh22@myune.edu.au>
2020-05-13 16:24:38 +1000

    Added splash title textview, adjustments to splash layout

--- app/src/main/java/android/example/cyclesave/SplashScreenActivity.kt
+++ app/src/main/java/android/example/cyclesave/SplashScreenActivity.kt
@@ -7,7 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    private val splashQuotes = listOf("Save time and money","Stay fit!")
+    private val splashQuotes = listOf(
+        "In total, riding your bike accounts for about 21g of CO2 emissions per kilometer, less than 10x that of a car!",
+        "Cycling can help to protect you from serious diseases such as stroke, heart attack, some cancers, depression, diabetes, obesity and arthritis.",
+        "Steady cycling burns about 1,200 kilojoules (about 300 calories) per hour.")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
