51c8f7e8481f9157be2b72bbc446db41dae168c9
Russell Knight <rknigh22@myune.edu.au>
2020-05-13 13:27:49 +1000

    Added random splash quote selector

--- app/src/main/java/android/example/cyclesave/SplashScreenActivity.kt
+++ app/src/main/java/android/example/cyclesave/SplashScreenActivity.kt
@@ -1,12 +1,18 @@
 package android.example.cyclesave
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashQuotes = listOf("Save time and money","Stay fit!")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val textView = findViewById(R.id.splash_text) as TextView
+        textView.text = splashQuotes.random()
     }
 }
