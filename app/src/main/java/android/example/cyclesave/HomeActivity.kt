4d28bb5e728a28442b36cca0c5204ad717c77301
Russell Knight <rknigh22@myune.edu.au>
2020-05-20 23:19:53 +1000

    Added view costs button functionality

--- app/src/main/java/android/example/cyclesave/HomeActivity.kt
+++ app/src/main/java/android/example/cyclesave/HomeActivity.kt
@@ -1,7 +1,10 @@
 package android.example.cyclesave
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 // import FloatingButton here?
 
 class HomeActivity : AppCompatActivity() {
@@ -9,6 +12,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val costs_button = findViewById<Button>(R.id.button_view_costs)
+        costs_button.setOnClickListener{
+            val costs_intent = Intent(this, CostsActivity::class.java)
+            startActivity(costs_intent)
+        }
     }
 
     override fun onPause() {
