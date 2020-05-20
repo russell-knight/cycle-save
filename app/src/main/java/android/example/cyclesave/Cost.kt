3e731b119e66c916eb8ca9c1988297703d350150
Russell Knight <rknigh22@myune.edu.au>
2020-05-21 00:09:03 +1000

    Created Cost class

---
+++ app/src/main/java/android/example/cyclesave/Cost.kt
@@ -0,0 +1,10 @@
+package android.example.cyclesave
+
+import java.time.LocalDate
+
+/**The Cost class is used to store data about costs incurred from cycling
+ * e.g name=Service, price=100, datePurchased=03/12/20*/
+
+class Cost(var name: String, var price: Number, var datePurchased: LocalDate) {
+
+}
