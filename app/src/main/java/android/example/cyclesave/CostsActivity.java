8dbffbbe8c872e75e962649dcb3350dcf12bade9
Russell Knight <rknigh22@myune.edu.au>
2020-05-20 23:48:35 +1000

    Logical grouping of categories on Home Activity

--- app/src/main/java/android/example/cyclesave/CostsActivity.java
+++
@@ -1,6 +0,0 @@
-package android.example.cyclesave;
-
-import android.app.Activity;
-
-public class CostsActivity extends Activity {
-}
---
+++ app/src/main/java/android/example/cyclesave/CostsActivity.kt
@@ -0,0 +1,7 @@
+package android.example.cyclesave;
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CostsActivity : AppCompatActivity() {
+
+}
