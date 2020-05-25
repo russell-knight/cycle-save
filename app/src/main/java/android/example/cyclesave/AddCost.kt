bcce2bf0b05812fd7cb374bbab9fa4c9baed6333
Russell Knight <rknigh22@myune.edu.au>
2020-05-25 22:05:41 +1000

    Changed Add Cost date selector so that it change the textView text to the date selected

--- app/src/main/java/android/example/cyclesave/AddCost.kt
+++ app/src/main/java/android/example/cyclesave/AddCost.kt
@@ -3,10 +3,12 @@ package android.example.cyclesave
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_add_cost.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class AddCost : AppCompatActivity() {
@@ -19,21 +21,26 @@ class AddCost : AppCompatActivity() {
         toolbar.title = "Add Cost"
         setSupportActionBar(toolbar)
 
+        val textView: TextView = findViewById(R.id.editText_date)
+        textView.text = SimpleDateFormat("dd/MM/yyyy").format(System.currentTimeMillis())
 
+        var cal = Calendar.getInstance()
 
-        // Prevent keyboard from appearing when date field is selected
-        editText_date.showSoftInputOnFocus = false
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
-        editText_date.setOnClickListener {
-            val now = Calendar.getInstance()
-            val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                Toast.makeText(this, "Year : " + year
-                        + "\nMonth :" + month
-                + "\nDay : " + dayOfMonth,Toast.LENGTH_SHORT) .show()
-            },
-
-                    now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH))
-            datePicker.show()
+                val myFormat = "dd/MM/yyyy" // mention the format you need
+                val sdf = SimpleDateFormat(myFormat, Locale.US)
+                textView.text = sdf.format(cal.time)
+            }
+        textView.setOnClickListener {
+            DatePickerDialog(this@AddCost, dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
         }
     }
 }
