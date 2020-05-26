c3edae22eef05bba91a84808217e40b926778934
Russell Knight <rknigh22@myune.edu.au>
2020-05-26 11:15:47 +1000

    Refactored Add Cost button to Floating Action Button

--- app/src/main/java/android/example/cyclesave/AddCost.kt
+++ app/src/main/java/android/example/cyclesave/AddCost.kt
@@ -2,12 +2,9 @@ package android.example.cyclesave
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import kotlinx.android.synthetic.main.activity_add_cost.*
 import java.text.SimpleDateFormat
 import java.util.*
 
