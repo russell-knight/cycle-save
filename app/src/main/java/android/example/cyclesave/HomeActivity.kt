8691c6d0f50f0535d26f4f44d20b4f36b33f91a6
Russell Knight <rknigh22@myune.edu.au>
2020-05-29 19:34:22 +1000

    Implemented increment and decrement trips button functionalities.

--- app/src/main/java/android/example/cyclesave/HomeActivity.kt
+++ app/src/main/java/android/example/cyclesave/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 // import FloatingButton here?
 
@@ -16,15 +18,27 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val costsButton = findViewById<Button>(R.id.button_view_costs)
-        costsButton.setOnClickListener{
+        // Set initial values
+        var tripsText = findViewById<TextView>(R.id.trips_taken_value)
+        tripsText.text = tripsTaken.toString()
+
+        val addTrip = findViewById<Button>(R.id.add_trip_button)
+        addTrip.setOnClickListener{
+            tripsTaken++
+            tripsText.text = tripsTaken.toString()
+        }
+
+        val subtractTrip = findViewById<Button>(R.id.subtract_trip_button)
+        subtractTrip.setOnClickListener{
+            tripsTaken--
+            tripsText.text = tripsTaken.toString()
+        }
+
+        // Set button to go to Costs Activity
+        val viewCostsBtn = findViewById<Button>(R.id.button_view_costs)
+        viewCostsBtn.setOnClickListener{
             val costsIntent = Intent(this, CostsActivity::class.java)
             startActivity(costsIntent)
         }
-    }
-
-    override fun onPause() {
-        super.onPause()
-
     }
 }
